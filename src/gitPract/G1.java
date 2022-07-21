d2c66d9ec8a466404e689de7b48de161cff087bc
jrgaikwad <gaikwadjeevan73@gmail.com>
2022-07-21 23:24:09 +0530

    First Commit

---
+++ src/gitPract/G1.java
@@ -0,0 +1,9 @@
+package gitPract;
+
+public class G1 {
+	public static void main (String [] args) {
+		 System.out.println("Assert 5 Started");
+		  String d = "Dinesh";
+		  String c = "Dinfdhgsesh";
+	}
+}
