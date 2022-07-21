1121937d884b4e466091cb918f119d712740cc36
jrgaikwad <102745702+jrgaikwad@users.noreply.github.com>
2022-07-22 00:30:19 +0530

    G1 msd

--- src/gitPract/G1.java
+++ src/gitPract/G1.java
@@ -2,8 +2,8 @@ package gitPract;
 
 public class G1 {
 	public static void main (String [] args) {
-		 System.out.println("Jeevan G1 Class");
-		  String d = "Bravo";
+		 System.out.println("Updated G1 Class");
+		  String d = "msd";
 		  String c = "Dinfdhgsesh";
 	}
 }
