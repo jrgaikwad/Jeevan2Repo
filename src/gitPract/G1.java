63a20f203d01a6c8914b74c72e7edf37fd38f61a
jrgaikwad <gaikwadjeevan73@gmail.com>
2022-07-22 00:32:33 +0530

    csk commit

--- src/gitPract/G1.java
+++ src/gitPract/G1.java
@@ -3,7 +3,9 @@ package gitPract;
 public class G1 {
 	public static void main (String [] args) {
 		 System.out.println("Jeevan G1 Class");
-		  String d = "Bravo";
+		  String d = "Jadeja";
 		  String c = "Dinfdhgsesh";
+		  String franch = "CSK";
 	}
 }
+
