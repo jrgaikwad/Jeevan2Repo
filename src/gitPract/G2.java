a4fcd5d789ede3ca6d42ceb66f94cb06a9030f83
jrgaikwad <gaikwadjeevan73@gmail.com>
2022-07-21 23:33:50 +0530

    G2 Commit

---
+++ src/gitPract/G2.java
@@ -0,0 +1,9 @@
+package gitPract;
+
+public class G2 {
+	public static void main (String [] args) {
+		 System.out.println("Class G2");
+		  String d = "Dinesh";
+		  String c = "Dinfdhgsesh";
+	}
+}
