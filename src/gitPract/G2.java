b772ffc6112929f8657e9dfd435e902392276c23
jrgaikwad <102745702+jrgaikwad@users.noreply.github.com>
2022-07-21 23:41:33 +0530

    Update G2.java

--- src/gitPract/G2.java
+++ src/gitPract/G2.java
@@ -2,8 +2,8 @@ package gitPract;
 
 public class G2 {
 	public static void main (String [] args) {
-		 System.out.println("Class G2");
-		  String d = "Dinesh";
-		  String c = "Dinfdhgsesh";
+		 System.out.println("Class G2 og Jeevan");
+		  String d = "Digvijay";
+		  String c = "Dj";
 	}
 }
